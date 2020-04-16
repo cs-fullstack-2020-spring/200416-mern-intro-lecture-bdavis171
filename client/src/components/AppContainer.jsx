6eefd91b934fbd606c653b36f3f7a049eae454c5
bdavis171 <bdavis171@gmail.com>
2020-04-16 10:15:56 -0500

    client side tested

--- client/src/components/AppContainer.jsx
+++ client/src/components/AppContainer.jsx
@@ -11,7 +11,7 @@ class AppContainer extends Component {
     render() { 
         return ( 
             <Fragment>
-                
+                <h1>hello</h1>
             </Fragment>
          );
     }
